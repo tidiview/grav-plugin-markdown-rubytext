ede261cb4144bc8714c53a044bf2ff2420e67b35
Sven Hüser <s.hueser@gmail.com>
2017-08-19 12:24:53 +0200

    restore old implementation for backwards compatibility

--- markdown-rubytext.php
+++ markdown-rubytext.php
@@ -2,6 +2,7 @@
 namespace Grav\Plugin;
 
 use \Grav\Common\Plugin;
+use RocketTheme\Toolbox\Event\Event;
 
 
 class MarkdownRubyTextPlugin extends Plugin
@@ -14,6 +15,7 @@ class MarkdownRubyTextPlugin extends Plugin
         return [
             'onShortcodeHandlers' => ['onShortcodeHandlers', 0],
             'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+            'onMarkdownInitialized' => ['onMarkdownInitialized', 0],
         ];
     }
 
@@ -31,5 +33,47 @@ class MarkdownRubyTextPlugin extends Plugin
     public function onShortcodeHandlers()
     {
         $this->grav['shortcode']->registerAllShortcodes(__DIR__.'/shortcodes');
+    }
+
+    /**
+     * old implementation without shortcode
+     */
+    public function onMarkdownInitialized(Event $event)
+    {
+        $markdown = $event['markdown'];
+        // Initialize Text example
+        $markdown->addInlineType('{', 'RubyText');
+        // Add function to handle this
+        $markdown->inlineRubyText = function($excerpt) {
+            if (preg_match('/\{r}([^\s{]+){\/r:([^\s}]+)}/', $excerpt['text'], $matches))
+            {
+                return
+                array(
+                  'extent' => strlen($matches[0]),
+                  'element' => array(
+                    'name' => 'ruby',
+                    'handler' => 'elements',
+                    'text' => array(
+                        array(
+                            'name' => 'rb',
+                            'text' => $matches[1],
+                            ),
+                        array(
+                            'name' => 'rp',
+                            'text' => '（',
+                            ),
+                        array(
+                            'name' => 'rt',
+                            'text' => $matches[2],
+                            ),
+                        array(
+                            'name' => 'rp',
+                            'text' => '）',
+                            )
+                        )
+                    )
+                );
+            }
+        };
     }
 }
